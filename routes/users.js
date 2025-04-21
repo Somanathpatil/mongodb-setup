a39f4768308e4279455e2ba389acf7e1fa3e6019
Somanath patil <patilsomanath292@gmail.com>
2025-04-21 09:56:36 +0530

    this is commit

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/practicekaro");       
+
+const userschema = mongoose.Schema({
+  username:{
+    type:String,
+    require:true
+  }
+});
+
+const exports = mongoose.model("exports",userschema); 
